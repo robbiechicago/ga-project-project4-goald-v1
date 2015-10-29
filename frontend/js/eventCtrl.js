dfca474a2e60609da89847266aef42998e398873
Rob Holloway <robbiechicago@gmail.com>
2015-10-29 11:41:57 +0000

    created post requests for adding new events.  Seems to work.  Yay me

--- frontend/js/eventCtrl.js
+++ frontend/js/eventCtrl.js
@@ -7,7 +7,28 @@ function eventCtrl($http, $state, $stateParams) {
   
   var self = this;
 
-  function newEvent() {
-    
+  self.newEvent = newEvent;
+  function newEvent(project_id, created_at, user_id) {
+    console.log('a nice new event');
+    console.log('project id: ' + project_id)
+    console.log('project created at ' + created_at)
+    console.log('user_id = ' +user_id)
+
+    var theDateTime = (new Date)
+    console.log(theDateTime)
+
+    var aNewEvent = {
+      "project_id": project_id,
+      "event_datetime": theDateTime
+    }
+
+    console.log(aNewEvent)
+
+    $http
+      .post('http://localhost:3000/users/' + user_id + '/projects/' + project_id + '/events', aNewEvent)
+      .then(function(response) {
+        console.log(response);
+    });
+
   }
 }
--- v1_api/app/controllers/events_controller.rb
+++ v1_api/app/controllers/events_controller.rb
@@ -1,6 +1,17 @@
 class EventsController < ApplicationController
 
+  def create
+    @event = Event.new(project_id: params[:project_id], event_datetime: params[:event_datetime])
 
+    if @event.save
+      puts 'saved'
+      # render json: @event, status: :created, location: @event
+      
+    else
+      puts 'not saved'
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
 
   
 
